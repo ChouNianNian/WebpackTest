17bba9f0cde4ce7f9b7044ef70e53616882df644
研发中心-谢余飞17469 <xyf17469@tcent.cn>
2016-10-21 11:56:38 +0800

    支持es6

--- public/javascript/entry.js
+++ public/javascript/entry.js
@@ -1,4 +1,17 @@
-require("../stylesheet/style.css");
-let a=10;
-document.write('It works.'+a);
-document.write(require('./module.js'));
+require('../stylesheet/style.css');
+let text= require('./module.js');
+let array = [1, 2, 3, 4];
+
+class Person{
+	constructor(name){
+        this.name = name;
+    }
+    sayHello(){
+    	console.log(`hi I am ${this.name} `);
+    }
+}
+let person=new Person('xyf')
+person.sayHello();
+
+document.write('It works.' + array[0]);
+document.write(text);
--- webpack.config.js
+++ webpack.config.js
@@ -8,12 +8,18 @@ module.exports = {
     },
     output: {
         path: path.resolve(__dirname, 'build'),
-        filename: '[name].bundle.js'
+        filename: '[name].bundle.js',
+        publicPath: './build/'
     },
     module: {
         loaders: [
             { test: /\.css$/, loader: 'style!css' },
-            { test: /\.(png|jpg|gif)$/, loader: 'file-loader?name=../public/images/[name].[ext]' }
+            { test: /\.(png|jpg|gif)$/, loader: 'file-loader?limit=8192&name=../../public/images/[name].[ext]' },
+            { test: /\.js[x]?$/, loader: 'babel', exclude: /node_modules/ }
         ]
-    }
+    },
+    plugins: [
+        //查找相等或近似的模块，避免在最终生成的文件中出现重复的模块
+        // new webpack.optimize.DedupePlugin()
+    ]
 }
