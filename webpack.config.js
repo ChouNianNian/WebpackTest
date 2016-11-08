069ab37aa29cd6bf454cd52e188f69214ef243bd
acexyf <2817259608@qq.com>
2016-11-08 22:46:14 +0800

    添加script

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,6 @@ let ExtractTextPlugin = require("extract-text-webpack-plugin");
 module.exports = {
     entry: {
         index: './public/javascript/entry.js'
-
     },
     output: {
         path: path.resolve(__dirname, 'build'),
@@ -14,15 +13,14 @@ module.exports = {
     module: {
         loaders: [
             { test: /\.css$/, loader: ExtractTextPlugin.extract('style-loader', 'css-loader') },
-            { test: /\.(png|jpg|gif)$/, loader: 'file-loader?limit=8192&name=../../public/images/[name].[ext]' },
+            { test: /\.(png|jpg|gif)$/, loader: 'file-loader?limit=8192&name=../[path][name].[ext]' },
             { test: /\.js[x]?$/, loader: 'babel' }
         ]
     },
     plugins: [
         //查找相等或近似的模块，避免在最终生成的文件中出现重复的模块
-        // new webpack.optimize.DedupePlugin()
-        new webpack.optimize.CommonsChunkPlugin('common.js'),
-        new webpack.optimize.UglifyJsPlugin(),
+        // new webpack.optimize.CommonsChunkPlugin('common.js'),
+        // new webpack.optimize.UglifyJsPlugin(),
         new ExtractTextPlugin('[name].css', { allChunks: true })
     ]
 }
